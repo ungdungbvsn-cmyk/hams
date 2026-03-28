d322eefecef3596b17eae96bb3734d85be35f31f
ungdungbvsn-cmyk <ungdungbvsn@gmail.com>
2026-03-28 14:19:04 +0700

    Update prisma.ts

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -2,16 +2,13 @@ import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+    var prisma: PrismaClient | undefined;
 }
-
-const DATABASE_URL = 'postgresql://postgres.cvpzrimpdbputhapanjf:Sn123456%23@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true&connection_limit=1';
-process.env.DATABASE_URL = DATABASE_URL;
 
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
+    global.prisma = prisma;
 }
 
 export default prisma;
